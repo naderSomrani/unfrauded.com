598bcba2ec07bb6fdff6fdf527695250167e9bfa
Nader <nader.somrani1996@gmail.com>
2022-02-10 16:11:50 +0100

    v1

---
+++ static/js/observers.js
@@ -0,0 +1,26 @@
+const header = document.querySelector(".navbar");
+const sectionOne = document.querySelector(".hero");
+const navigationButton = document.querySelector(".button-navigation");
+
+const sectionOneOptions = {
+  rootMargin: "-600px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      console.log(navigationButton)
+      header.classList.add("nav-scrolled");
+      navigationButton.classList.add("button-shrunk")
+    } else {
+      header.classList.remove("nav-scrolled");
+      navigationButton.classList.remove("button-shrunk")
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
